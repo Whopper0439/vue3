4518de155ff9c67e49a28461cea3695485800e26
whopper <whopper0439@gmail.com>
2026-04-10 02:05:23 +0800

    reactive:嵌套代理proxy

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -11,6 +11,12 @@ export function reactive(target) {
  */
 const reactiveMap = new WeakMap()
 
+/**
+ * 保存所有使用reactive创建出来的响应式对象
+ */
+
+const reactiveSet = new WeakSet()
+
 function createReactiveObject(target) {
   /**
    * reactive必须接受一个对象
@@ -27,6 +33,13 @@ function createReactiveObject(target) {
   const existingProxy = reactiveMap.get(target)
   if (existingProxy) {
     return existingProxy
+  }
+
+  /**
+   * 检查target是否已经是响应式对象,存入到了reactiveSet，直接返回
+   */
+  if (reactiveSet.has(target)) {
+    return target
   }
 
   // target是一个对象，创建target的代理对象
@@ -59,6 +72,10 @@ function createReactiveObject(target) {
 
   //保存 target 和 proxy响应式对象 之间的关联关系
   reactiveMap.set(target, proxy)
+
+  //保存响应式对象到reactiveSet
+  reactiveSet.add(proxy)
+
   return proxy
 }
 
@@ -139,4 +156,13 @@ class Dep {
   subs: Link
   subsTail: Link
   constructor() {}
+}
+
+/**
+ * 判断target是不是响应式对象，只要在reactiveSet中即可
+ * @param target
+ * @return
+ */
+export function isReactive(target) {
+  return reactiveSet.has(target)
 }
