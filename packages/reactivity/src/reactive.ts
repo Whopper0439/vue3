c4a2c2a74db40b962bd88647197ca1f3af252136
whopper <whopper0439@gmail.com>
2026-04-10 14:20:52 +0800

    reactive:ref被reactive包裹，更新解包的问题

--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -1,6 +1,7 @@
 import { isObject, hasChange } from '@vue/shared'
 import { Link, link, propagate } from './system'
 import { activeSub } from './effect'
+import { isRef } from './ref'
 
 export function reactive(target) {
   return createReactiveObject(target)
@@ -15,10 +16,18 @@ const mutableHandlers = {
     //console.log('get  target key:', target, key)
     track(target, key)
 
+    /**
+     * 如果target.a是一个ref，直接把值给他，不需要 .value
+     */
+    const res = Reflect.get(target, key, receiver) //返回target[key]
+    if (isRef(res)) {
+      return res.value
+    }
+
     // console.log(receiver === proxy)
 
     // receiver即传给count的this,使访问器中的this指向proxy代理对象
-    return Reflect.get(target, key, receiver) //返回target[key]
+    return res //返回target[key]
   },
   set(target, key, newValue, receiver) {
     const oldValue = target[key]
@@ -30,6 +39,18 @@ const mutableHandlers = {
 
     // 先更新set，再通知重新执行
     const res = Reflect.set(target, key, newValue, receiver)
+
+    /**
+     * 如果更新了state.a，他之前是一个ref,那么会修改原始的ref.value的值为newValue
+     * 如果newValue是一个ref,那就不需要同步
+     * const a = ref(0)
+     * target = { a }
+     * 更新了target.a = 1,就等于更新了 a.value = 1
+     */
+    if (isRef(oldValue) && !isRef(newValue)) {
+      oldValue.value = newValue
+      return res
+    }
 
     //如果set新值和老值不一样，触发更新
     if (hasChange(newValue, oldValue)) {
