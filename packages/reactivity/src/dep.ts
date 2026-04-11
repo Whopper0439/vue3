1562a78d5181bf4e548f29a23146bf3ba1b3bfd7
whopper <whopper0439@gmail.com>
2026-04-11 16:06:42 +0800

    reactive的数组length显示响应式

--- packages/reactivity/src/dep.ts
+++ packages/reactivity/src/dep.ts
@@ -61,16 +61,44 @@ export function trigger(target, key) {
     return
   }
 
-  const dep = depsMap.get(key)
-  if (!dep) {
-    /**
-     * dep没有，表示这个key从没有在sub中访问过，即没和effect建立过依赖
-     */
-    return
-  }
+  // /**
+  //  * 更新数组的 length
+  //  * 更新前：length = 4 => ['a', 'b', 'c', 'd']
+  //  * 更新后：length = 2 => ['a', 'b']
+  //  * 得出结论：要通知 访问了 c 和 d 的 effect 重新执行，就是访问了大于等于 length 的索引
+  //  * depsMap={  针对数组
+  //  *   0:dep
+  //  *   1:dep
+  //  *   2:dep
+  //  *   length:dep
+  //  *}
+  //  */
+  const targetIsArray = Array.isArray(target)
+  if (targetIsArray && key === 'length') {
+    // 处理数组的 length 更新
+    const length = target.length
+    depsMap.forEach((dep, depKey) => {
+      /**
+       * 通知 访问了 >= length 的 effect 重新执行
+       * 通知 访问了 length 属性的 effect 重新执行
+       */
+      if (depKey >= length || depKey === 'length') {
+        propagate(dep.subs)
+      }
+    })
+  } else {
+    // 不是 数组 ，或者 更新的不是数组length属性
+    const dep = depsMap.get(key)
+    if (!dep) {
+      /**
+       * dep没有，表示这个key从没有在sub中访问过，即没和effect建立过依赖
+       */
+      return
+    }
 
-  //找到dep的subs，通知他们重新执行
-  propagate(dep.subs)
+    //找到dep的subs，通知他们重新执行
+    propagate(dep.subs)
+  }
 }
 
 class Dep {
